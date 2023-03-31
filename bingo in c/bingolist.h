861ea03c41013c15c7670e2476631331226460b6
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2023-03-31 02:53:53 -0400

    created bingolist.h

---
+++ bingo in c/bingolist.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+typedef void *list;
+void set(list **, int, int ) ;
+void print(list *a) ; //og was complex instead of list
